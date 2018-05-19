b44fd12d1ebee38d666436294294942b30841da4
Vasiliy.Kamagorov <likanich152@gmail.com>
2018-05-19 15:29:12 +0700

    add bin

---
+++ bin/CMakeFiles/masonry.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Qt/src/main.cpp" "D:/Qt/bin/CMakeFiles/masonry.dir/main.cpp.obj"
+  "D:/Qt/src/mainwindow.cpp" "D:/Qt/bin/CMakeFiles/masonry.dir/mainwindow.cpp.obj"
+  "D:/Qt/src/masonry.cpp" "D:/Qt/bin/CMakeFiles/masonry.dir/masonry.cpp.obj"
+  "D:/Qt/bin/masonry_autogen/mocs_compilation.cpp" "D:/Qt/bin/CMakeFiles/masonry.dir/masonry_autogen/mocs_compilation.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "D:/Qt/src"
+  "masonry_autogen/include"
+  "C:/Qt/5.10.1/mingw53_32/include"
+  "C:/Qt/5.10.1/mingw53_32/include/QtWidgets"
+  "C:/Qt/5.10.1/mingw53_32/include/QtGui"
+  "C:/Qt/5.10.1/mingw53_32/include/QtANGLE"
+  "C:/Qt/5.10.1/mingw53_32/include/QtCore"
+  "C:/Qt/5.10.1/mingw53_32/./mkspecs/win32-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bin/CMakeFiles/masonry.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "masonry_autogen"
+  "CMakeFiles/masonry_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/masonry.dir/main.cpp.obj"
+  "CMakeFiles/masonry.dir/mainwindow.cpp.obj"
+  "CMakeFiles/masonry.dir/masonry.cpp.obj"
+  "CMakeFiles/masonry.dir/masonry_autogen/mocs_compilation.cpp.obj"
+  "masonry.pdb"
+  "masonry.exe"
+  "masonry.exe.manifest"
+  "libmasonry.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/masonry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
